2e2a01986f9655e27332f667b917cac430b0014b
dave <davidldenton@gmail.com>
2018-12-06 14:52:21 -0500

    added plots

--- ba_spread.R
+++ ba_spread.R
@@ -1,12 +1,18 @@
 library(tidyverse)
 library(reticulate)
+library(caret)
+library(gridExtra)
 
+# Import list of tickers from Excel
 ticker_dat <- read_csv('tickers.csv')
 
+# Create function to get stick prices from Yahoo API
 get_prices <- function(ticker){
-    tidyquant::tq_get(ticker, get = "stock.prices", from = '1990-01-02') %>% mutate(ticker = ticker)
+    tidyquant::tq_get(ticker, get = "stock.prices", from = '1990-01-02', to = '2018-11-27') %>% 
+        mutate(ticker = ticker)
 }
 
+# Download stock prices, calculate spread, and lag the spread by one day
 features <- map_df(ticker_dat$ticker, get_prices) %>%
     mutate(ba_spread = high - low,
            ba_spread_pct = ba_spread/((high+low)/2)) %>%
@@ -20,7 +26,7 @@ features <- map_df(ticker_dat$ticker, get_prices) %>%
 #write_csv(spread_dat, 'spread_dat.csv', na = '')
 #spread_dat <- read_csv('spread_dat.csv')
 
-
+# Read VIX data downloaded from St. Louis Federal Reserve
 target <- bind_rows(read_csv('vixarchive.csv'), read_csv('vixcurrent.csv')) %>%
     mutate(vix_delta = vix_close - lag(vix_close, n = 1, order_by = date),
            vix_change = case_when(
@@ -28,18 +34,17 @@ target <- bind_rows(read_csv('vixarchive.csv'), read_csv('vixcurrent.csv')) %>%
                vix_delta <= 0 ~ 'lower volatility'
            )) %>%
     slice(-1) %>%
-    select(date, vix_change) %>%
-    filter(date %in% target$date)
+    select(date, vix_change)
 
 #write_csv(vix_dat, 'vix_dat.csv', na = '')
 #vix_dat <- read_csv('vix_dat.csv')
 
+# Collect data for some basic line plots to identify trends
 plot_dat <- inner_join(features, target, by = 'date') %>%
     gather(key = 'ticker', value = 'ba_spread_pct', AAPL:T) %>%
     filter(lubridate::year(date) == 2018)
 
-
-
+# Write function to create many plots at once
 plot_func <- function(abbrev){
     ggplot(data = plot_dat %>% filter(ticker == abbrev)) +
         geom_point(mapping = aes(x = date, y = ba_spread_pct, color = vix_change)) +
@@ -47,36 +52,64 @@ plot_func <- function(abbrev){
         ggtitle(label = abbrev)
 }
 
+# Create plots
 ggs <- map(unique(plot_dat$ticker), plot_func)
 
 
-
-# import/inspect the data
-# target <- read.csv('vix_dat.csv')
-target <- target[target$date %in% features$date,]
-target[1:10,]
-
-# features <- read.csv('price_dat.csv')
-features <- features[features$date %in% target$date,]
-features[1:10,]
-
-# quick check on join criteria
-obs_target <- nrow(target)
-obs_features <- nrow(features)
-countd_target_dts <- length(unique(target$date))
-countd_features_dts <- length(unique(features$date))
-
-countd_target_dts == countd_features_dts & obs_target == obs_features
-
 # create the dataset for python ML 
-model_df <- target %>% inner_join(features, by = c('date'))
-model_df[1:10,]
-summary(model_df)
+model_df <- target %>% inner_join(features, by = c('date')) %>%
+    mutate(vix_change = as_factor(vix_change))
+
 
 # Drop the date column as it is unecessary for modeling.
 # David encompased the temporal nature of the data in the calculation of vix_change.
 model_df <- model_df %>% select(-date)
-model_df[1:10,]
+
+# Create logistic regression model
+# set.seed(323)
+# intrain <- createDataPartition(y = model_df$vix_change, p = 0.80, list = FALSE)
+# training <- model_df[intrain,]
+# testing <- model_df[-intrain,]
+# 
+# insubtrain <- createDataPartition(y = training$vix_change, p = 0.80, list = FALSE)
+# training_test <- training[-insubtrain,]
+# training_train <- training[insubtrain,]
+
+# Create control for train (10-fold cross validation)
+# control <- trainControl(method = "repeatedcv", number = 10, repeats = 2, verboseIter = FALSE)
+
+# GLM - Logistic Regression
+# fit_glm <- train(vix_change ~ ., method = "glm", family = "binomial", data = training_train, trControl = control)
+
+#GLM - Logistic Regression
+# predict_glm <- predict(fit_glm, newdata = training_test)
+# test_glm <- confusionMatrix(predict_glm, training_test$vix_change)
+
+# ggplot of confusion matrix and table of accuracy & kappa stats
+# glm_plot <- ggplot(training_test, aes(x = predict_glm, y = vix_change)) + 
+#     geom_jitter(width = 0.3, height = 0.3, alpha = 0.5, color = "dark blue") +
+#     labs(title = "GLM", x = "prediction", y = "actual")
+# 
+# data.frame(model = "GLM",
+#            accuracy = test_glm$overall[[1]],
+#            kappa = test_glm$overall[[2]])
+
+# Fit final model
+# fit_glm <- train(vix_change ~ ., method = "glm", family = "binomial", data = training, trControl = control)
+# predict_glm <- predict(fit_glm, newdata = testing)
+# cm_glm <- confusionMatrix(predict_glm, testing$vix_change)
+# 
+# g_glm <- ggplot(testing, aes(x = predict_glm, y = vix_change)) + 
+#     geom_jitter(width = 0.3, height = 0.3, alpha = 0.5, color = "dark blue") +
+#     labs(title = "GLM", x = "prediction", y = "actual")
+# 
+# data.frame(model = "GLM",
+#            accuracy = cm_glm$overall[[1]],
+#            kappa = cm_glm$overall[[2]])
+
+
+# Create XGBoost model in Python
+
 
 # had to create project level .RProfile via terminal and and set the value of RETICULATE_PYTHON in order to get
 # reticulate to use the correct installation of python (anaconda dist).  use_python() did not work
@@ -86,11 +119,11 @@ py_config()
 # use_condaenv(condaenv='reticulate_demo_env', conda='/Users/adamrosenblit/anaconda3/bin/conda')
 # py_config()
 # py_discover_config()
-
 # conda install -y -c conda-forge xgboost
 # pip install xgboost
 # py_install('xgboost')
 # https://rstudio.github.io/reticulate/articles/python_packages.html
+ 
 pd <- import('pandas')
 np <- import('numpy')
 xgb <- import('xgboost')
@@ -119,7 +152,6 @@ y_test <- test_train_split[[4]]
 
 # fit model on training data, using default params
 # https://xgboost.readthedocs.io/en/latest/parameter.html
-# 
 model_0 <- xgb$XGBClassifier(objective='binary:logistic')
 model_0$fit(X_train, y_train)
 
@@ -128,7 +160,7 @@ model_0_y_pred <- model_0$predict(X_test)
 
 # evaluate predictions
 model_0_accuracy <- mtx$accuracy_score(y_test, model_0_y_pred)
-print(paste('Accuracy: %', round(model_0_accuracy * 100.0,2), sep=''))
+print(paste('Accuracy: ', round(model_0_accuracy * 100.0,2), '%', sep=''))
 
 # plot feature importance (ie, num splits per feature)
 # https://stackoverflow.com/questions/34218245/how-is-the-feature-score-importance-in-the-xgboost-package-calculated
@@ -136,13 +168,15 @@ print(paste('Accuracy: %', round(model_0_accuracy * 100.0,2), sep=''))
 # xgb$plot_importance(model.0)
 # plt$show()
 # https://towardsdatascience.com/interpreting-random-forest-and-other-black-box-models-like-xgboost-80f9cc4a3c38
-model_0_feature_importances <- arrange(data.frame(feature_name=colnames(X), importance=model_0$feature_importances_),desc(importance))
-model_0_feature_importances 
-# https://machinelearningmastery.com/feature-importance-and-feature-selection-with-xgboost-in-python/
 
+model_0_feature_importances <- arrange(data.frame(feature_name=colnames(X), importance=model_0$feature_importances_),desc(importance))
+model_0_feature_importances
+
+# https://machinelearningmastery.com/feature-importance-and-feature-selection-with-xgboost-in-python/
 # https://xgboost.readthedocs.io/en/latest/parameter.html
 # https://towardsdatascience.com/fine-tuning-xgboost-in-python-like-a-boss-b4543ed8b1e
 # https://rstudio.github.io/reticulate/reference/dict.html
+
 keys <- list(
     'n_estimators', 
     'learning_rate', 
@@ -167,6 +201,7 @@ params <- py_dict(keys, values, convert = TRUE)
 # nthreads=-1 allows xgboost to parallelize training across processors
 # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.RandomizedSearchCV.html
 # http://danielhnyk.cz/how-to-use-xgboost-in-python/
+
 model_1 <- xgb$XGBClassifier(objective='binary:logistic', nthreads=-1)
 tuned_model_1 <- ms$RandomizedSearchCV(model_1, param_distributions=params, n_jobs=1, n_iter=50L)
 tuned_model_1$fit(X_train, y_train)  
@@ -175,10 +210,36 @@ best_estimator_m1
 
 best_estimator_m1_y_pred <- best_estimator_m1$predict(X_test)
 best_estimator_m1_accuracy <- mtx$accuracy_score(y_test, best_estimator_m1_y_pred)
-print(paste('Accuracy: %', round(best_estimator_m1_accuracy * 100.0,2), sep=''))
+print(paste('Accuracy: ', round(best_estimator_m1_accuracy * 100.0,2), '%', sep=''))
 
+
+# Create ggplot2 confusion matrix plot
+xg_plot <- bind_cols(prediction = best_estimator_m1_y_pred, actual = y_test) %>%
+    mutate(accuracy = ifelse(prediction == actual, 'TRUE', 'FALSE')) %>%
+ggplot(aes(x = best_estimator_m1_y_pred, y = y_test, color = accuracy)) + 
+    geom_jitter(width = 0.3, height = 0.3, alpha = 0.5, size = 2) +
+    labs(title = "XGBoost", x = "prediction", y = "actual") +
+    scale_color_manual(values=c("#00008B", "#FF4500")) +
+    theme(axis.title.y = element_text(size = 16), axis.text.y = element_text(size = 14)) +
+    theme(axis.title.x = element_text(size = 16), axis.text.x = element_text(size = 14)) +
+    theme(plot.title = element_text(size = 18, hjust = 0.5))
+xg_plot
+
+
+# Create data frame that contains feature importances
 best_estimator_m1_feature_importances <- arrange(data.frame(feature_name=colnames(X), importance=best_estimator_m1$feature_importances_),desc(importance))
 best_estimator_m1_feature_importances
+
+
+# Plot feature importances
+feature_plot <- best_estimator_m1_feature_importances %>%
+    ggplot(aes(x = reorder(feature_name, -importance), y = importance)) + 
+    geom_bar(stat = 'identity', color = 'dark red', fill = 'dark red') +
+    labs(title = "Feature importance", x = "ticker", y = "importance") +
+    theme(axis.title.y = element_text(size = 16), axis.text.y = element_text(size = 14)) +
+    theme(axis.title.x = element_text(size = 16), axis.text.x = element_text(size = 14)) +
+    theme(plot.title = element_text(size = 18, hjust = 0.5))
+feature_plot
 
 # leverage the pickle class from python by way of two reticulate functions, py_save_object and py_load_object
 # https://github.com/rstudio/reticulate/blob/master/R/pickle.R
@@ -189,6 +250,8 @@ py_save_object(decoder, 'decoder.df')
 
 # saving this off as an R object
 dump(c('best_estimator_m1_feature_importances'), 'feature_importance.df')
+
+
 
 # or, run a python session from R console
 # repl_python()
